b31b11d3ad2b946190be2a732ea6bea73be751d9
root <root@ip-172-31-30-54.ec2.internal>
2017-08-21 14:42:00 +0000

    first commit

---
+++ MynewProj/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MynewProj.MvcApplication" Language="C#" %>
---
+++ MynewProj/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MynewProj.Startup))]
+namespace MynewProj
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
